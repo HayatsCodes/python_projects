68c47144b7ec3078bc3a8bc06d45a9277f90afc1
Hayats <hayatudeenabdulrahman@gmail.com>
2023-01-02 21:33:05 +0100

    email_sender

---
+++ 0-email_sender.py
@@ -0,0 +1,27 @@
+# zhacksymcamewxwl
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'hayatscodes@gmail.com'
+email_password = 'zhacksymcamewxwl'
+
+# Generated from https://temp-mail.org/en/
+email_receiver = 'bahoji3076@cnxcoin.com'
+subject = "Don't forget to subscribe"
+
+body = """
+When you watch a video, please hit subscribe
+"""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+mail_context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=mail_context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
